7ddb16c6c60c16cf469f34102e1398d2de32d60f
olegnovatskiy <olegnovatskiy@gmail.com>
2017-01-13 16:08:34 +0200

    Create project

---
+++ src/main/java/com/shopcar/ShopCars5Application.java
@@ -0,0 +1,12 @@
+package com.shopcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCars5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCars5Application.class, args);
+	}
+}
